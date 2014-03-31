3019599bcbfe70af86f29945e30cf4a569126d87
jollig <eyjolfur02@ru.is>
2014-03-31 22:15:48 +0000

    fooling around with the affector

--- OrbitingAffector.cpp
+++ OrbitingAffector.cpp
@@ -2,16 +2,61 @@
 
 using namespace Ogre;
 
+// Instantiate statics
+OrbitingAffector::CmdColor OrbitingAffector::msColorCmd;
+
 OrbitingAffector::OrbitingAffector(ParticleSystem* psys) :ParticleAffector(psys)
 {
 	mType = "Orbiting";
 
+	// defaults
+	mCol.setAsRGBA(1);
 
+	// Setup parameters
+	if(createParamDictionary("Orbiting")) {
+		addBaseParameters();
+		ParamDictionary* dict = getParamDictionary();
+
+		dict->addParameter(ParameterDef("color", "Determine color of particles",
+			PT_COLOURVALUE), &msColorCmd);
+	}
 }
 
 OrbitingAffector::~OrbitingAffector(void)
 {
 }
 
-void OrbitingAffector::_affectParticles(ParticleSystem* pSystem, Real timeElapsed)
-{}
+void OrbitingAffector::_affectParticles(ParticleSystem* pSystem, Real timeElapsed) {
+	ParticleIterator pi = pSystem->_getIterator();    
+	Particle* p;
+
+	
+}
+
+//-----------------------------------------------------------------------
+void OrbitingAffector::setColor(const ColourValue& col)
+{
+    mCol = col;
+}
+//-----------------------------------------------------------------------
+ColourValue OrbitingAffector::getColor(void) const
+{
+    return mCol;
+}
+//-----------------------------------------------------------------------
+
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+// Command objects
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+String OrbitingAffector::CmdColor::doGet(const void* target) const
+{
+    return StringConverter::toString(
+        static_cast<const OrbitingAffector*>(target)->getColor() );
+}
+void OrbitingAffector::CmdColor::doSet(void* target, const String& val)
+{
+    static_cast<OrbitingAffector*>(target)->setColor(
+        StringConverter::parseColourValue(val));
+}
--- OrbitingAffector.h
+++ OrbitingAffector.h
@@ -4,6 +4,7 @@
 
 #include "OgreParticleFXPrerequisites.h"
 #include "OgreParticle.h"
+#include "OgreParticleSystem.h"
 #include "OgreStringConverter.h"
 #include "OgreParticleAffector.h"
 
@@ -12,11 +13,28 @@ using namespace Ogre;
 class OrbitingAffector : public ParticleAffector
 {
 public:
+	/** Command object for color (see ParamCommand).*/
+    class CmdColor : public ParamCommand
+    {
+    public:
+        String doGet(const void* target) const;
+        void doSet(void* target, const String& val);
+    };
+
 	OrbitingAffector(ParticleSystem* psys);
 
 	~OrbitingAffector(void);
 
 	void _affectParticles(ParticleSystem* pSystem, Real timeElapsed);
+
+	void setColor(const ColourValue& col);
+	ColourValue getColor(void) const;
+
+	/// Command objects
+	static CmdColor msColorCmd;
+
+protected:
+	ColourValue mCol;
 };
 
 #endif
